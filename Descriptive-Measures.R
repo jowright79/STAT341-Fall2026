6f81e1ef1802bfb706c8aafb0387cbee8c2845b7
jowright79 <jowright@claflin.edu>
2026-08-18 16:46:10 -0400

    Add files via upload

---
+++ Descriptive-Measures.R
@@ -0,0 +1,22 @@
+#Name:Jordane Wright, Date:08/18/2026, Purpose:To test functions of descriptive 
+
+#Create a variable called variable1 and store 5 numbers from 1-5
+> variable1 <- c(1,2,3,4,5)
+> variable1
+[1] 1 2 3 4 5
+
+# Calculate mean of variable1
+> mean(variable1)
+[1] 3
+
+#Calculate standard deviation
+> sd(variable1)
+[1] 1.581139
+
+#Calculate median
+> median(variable1)
+[1] 3
+
+#Calculate Mode
+> mode(variable1)
+[1] "numeric"
